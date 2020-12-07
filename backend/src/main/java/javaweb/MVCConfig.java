7c954a15651a78751feedb18ad239c1cf8ac57a4
Nguyen Hoang Viet <hoangviet0502.giangvo@gmail.com>
2020-12-07 22:02:40 +0700

    MVC COMPLETE

--- backend/src/main/java/javaweb/MVCConfig.java
+++ backend/src/main/java/javaweb/MVCConfig.java
@@ -8,8 +8,19 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 
+@Configuration
+public class MVCConfig extends WebMvcConfigurerAdapter {
 
-public class MVCConfig{
-	
+  @Override
+  public void addViewControllers(ViewControllerRegistry registry) {
+      registry.addViewController("/{spring:\\w+}")
+            .setViewName("forward:/");
+      registry.addViewController("/**/{spring:\\w+}")
+            .setViewName("forward:/");
+      registry.addViewController("/{spring:\\w+}/**{spring:?!(\\.js|\\.css)$}")
+            .setViewName("forward:/");
+  }
 }
